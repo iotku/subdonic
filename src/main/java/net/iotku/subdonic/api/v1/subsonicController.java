f591518f1d276f087f58c4404b52241021f8f5ea
Jonathan A. <me@iotku.pw>
2025-09-17 21:53:29 -0700

    feat: add search2 and search3 subsonic endpoints

--- src/main/java/net/iotku/subdonic/api/v1/subsonicController.java
+++ src/main/java/net/iotku/subdonic/api/v1/subsonicController.java
@@ -5,11 +5,13 @@ import net.beardbot.subsonic.client.base.SubsonicIncompatibilityException;
 import net.iotku.subdonic.subsonic.SubsonicConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.subsonic.restapi.Child;
+
+import java.util.List;
 
 @RestController
+@SuppressWarnings("unused")
 @RequestMapping("/api/v1/subsonic")
 public class subsonicController {
     private static final Logger log = LoggerFactory.getLogger(subsonicController.class);
@@ -21,17 +23,31 @@ public class subsonicController {
         this.subsonic = config.subsonic();
     }
 
-    @GetMapping("/ping")
-    public String ping() {
+    @GetMapping("/test")
+    public String test() {
         try {
             if (subsonic.testConnection()) {
                 log.info("Connected to server");
+                return "Connected";
             } else {
                 log.error("Failed to connect to {} with user {} and pass {}", config.getServerURL(), config.getUser(), config.getPass());
+                return "Not Connected";
             }
         } catch (SubsonicIncompatibilityException e) {
             log.error("Server not compatible with client");
+            return "Server Incompatible";
         }
-        return "";
+    }
+
+    @GetMapping("/search2")
+    @ResponseBody
+    public List<Child> search2(@RequestParam String query) {
+        return subsonic.searching().search2(query).getSongs();
+    }
+
+    @GetMapping("/search3")
+    @ResponseBody
+    public List<Child> search3(@RequestParam String query) {
+        return subsonic.searching().search3(query).getSongs();
     }
 }
