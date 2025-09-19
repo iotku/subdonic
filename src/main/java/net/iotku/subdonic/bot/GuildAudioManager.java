c2e27d89fac4b8bff35b74fac2e69240d4e5b2ce
Jonathan A. <me@iotku.pw>
2025-09-19 11:49:47 -0700

    feat: have GAM track VoicChannel connections for auto-disconnection

--- src/main/java/net/iotku/subdonic/bot/GuildAudioManager.java
+++ src/main/java/net/iotku/subdonic/bot/GuildAudioManager.java
@@ -6,9 +6,17 @@ import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
 import com.sedmelluq.discord.lavaplayer.track.playback.NonAllocatingAudioFrameBuffer;
 import discord4j.common.util.Snowflake;
+import discord4j.core.event.domain.VoiceStateUpdateEvent;
+import discord4j.core.object.VoiceState;
+import discord4j.core.object.entity.channel.VoiceChannel;
+import discord4j.core.spec.VoiceChannelJoinSpec;
 import discord4j.voice.VoiceConnection;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class GuildAudioManager {
@@ -42,6 +50,42 @@ public class GuildAudioManager {
         player.addListener(scheduler);
     }
 
+    public Mono<VoiceConnection> joinAndTrack(VoiceChannel channel) {
+        return channel.join(
+                        VoiceChannelJoinSpec.builder()
+                                .provider(getProvider())
+                                .selfDeaf(true)
+                                .build()
+                )
+                .doOnNext(this::setConnection)
+                .flatMap(conn -> {
+                    // === idle disconnect logic ===
+                    Publisher<Boolean> voiceStateCounter = channel.getVoiceStates()
+                            .count()
+                            .map(count -> count == 1L); // only the bot left
+
+                    Mono<Void> onDelay = Mono.delay(Duration.ofSeconds(10))
+                            .filterWhen(ignored -> voiceStateCounter)
+                            .switchIfEmpty(Mono.never())
+                            .then();
+
+                    Mono<Void> onEvent = channel.getClient().getEventDispatcher().on(VoiceStateUpdateEvent.class)
+                            .filter(vue -> vue.getOld()
+                                    .flatMap(VoiceState::getChannelId)
+                                    .map(channel.getId()::equals)
+                                    .orElse(false))
+                            .filterWhen(ignored -> voiceStateCounter)
+                            .next()
+                            .then();
+
+                    // Disconnect the connection when either fires
+                    return Mono.firstWithSignal(onDelay, onEvent)
+                            .then(conn.disconnect())
+                            .doFinally(sig -> setConnection(null)) // clear connection
+                            .thenReturn(conn); // still return the connection
+                });
+    }
+
     // getters
     public AudioPlayer getPlayer() {
         return player;
@@ -53,8 +97,8 @@ public class GuildAudioManager {
         return PLAYER_MANAGER;
     }
 
-    public VoiceConnection getVoiceConnection() {
-        return voiceConnection;
+    public Optional<VoiceConnection> getConnection() {
+        return Optional.ofNullable(this.voiceConnection);
     }
 
     // Setters
