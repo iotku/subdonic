70dd78c4691523928e14c2837d18271e125b82ff
Jonathan A. <me@iotku.pw>
2025-09-25 15:33:03 -0700

    chore: add toString() to MessageCtx

--- src/main/java/net/iotku/subdonic/bot/MessageCtx.java
+++ src/main/java/net/iotku/subdonic/bot/MessageCtx.java
@@ -1,6 +1,15 @@
 package net.iotku.subdonic.bot;
 
 import discord4j.common.util.Snowflake;
+import reactor.util.annotation.NonNull;
 
 public record MessageCtx(Snowflake guildId, Snowflake channelId, Snowflake memberId) {
+    @Override
+    public @NonNull String toString() {
+        return "MessageCtx{" +
+                "guildId=" + guildId.asLong() +
+                ", channelId=" + channelId.asLong() +
+                ", memberId=" + memberId.asLong() +
+                '}';
+    }
 }
