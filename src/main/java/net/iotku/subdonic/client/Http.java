ae2a4c10ccc9d8a101955de5afbc4cdbaacd5638
Jonathan A. <me@iotku.pw>
2025-09-29 13:31:29 -0700

    feat: add makeRequestStream to Http client
    Now we can return HttpResponse<InputStream> from the shared HttpClient

--- src/main/java/net/iotku/subdonic/client/Http.java
+++ src/main/java/net/iotku/subdonic/client/Http.java
@@ -1,6 +1,7 @@
 package net.iotku.subdonic.client;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -12,5 +13,10 @@ public class Http {
     public static HttpResponse<String> makeRequest(String url) throws IOException, InterruptedException {
         HttpRequest req = HttpRequest.newBuilder().uri(URI.create(url)).GET().build();
         return httpClient.send(req, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public static HttpResponse<InputStream> makeRequestStream(String url) throws IOException, InterruptedException {
+        HttpRequest req = HttpRequest.newBuilder().uri(URI.create(url)).GET().build();
+        return httpClient.send(req, HttpResponse.BodyHandlers.ofInputStream());
     }
 }
