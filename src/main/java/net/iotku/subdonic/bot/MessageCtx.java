be47d5d6e20ab9d975c308883fafaadf32961818
Jonathan A. <me@iotku.pw>
2025-10-02 13:47:26 -0700

    feat: create buildCtx MessageCtx factory method

--- src/main/java/net/iotku/subdonic/bot/MessageCtx.java
+++ src/main/java/net/iotku/subdonic/bot/MessageCtx.java
@@ -1,6 +1,8 @@
 package net.iotku.subdonic.bot;
 
 import discord4j.common.util.Snowflake;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Member;
 import reactor.util.annotation.NonNull;
 
 public record MessageCtx(Snowflake guildId, Snowflake channelId, Snowflake memberId) {
@@ -11,5 +13,9 @@ public record MessageCtx(Snowflake guildId, Snowflake channelId, Snowflake membe
                 ", channelId=" + channelId.asLong() +
                 ", memberId=" + memberId.asLong() +
                 '}';
+    }
+
+    public static MessageCtx buildCtx(MessageCreateEvent event) {
+        return new MessageCtx(event.getGuildId().orElse(null), event.getMessage().getChannelId(), event.getMember().map(Member::getId).orElse(null));
     }
 }
