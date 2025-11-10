ec8734365561d87eee152b9efe014c38daf28ee7
Jonathan A. <me@iotku.pw>
2025-11-09 17:52:37 -0800

    feat: actually use DiscordGuild DTO for addGuild

--- src/main/java/net/iotku/subdonic/ApiClient/Status.java
+++ src/main/java/net/iotku/subdonic/ApiClient/Status.java
@@ -1,6 +1,7 @@
 package net.iotku.subdonic.ApiClient;
 
 import discord4j.core.object.entity.Guild;
+import net.iotku.subdonic.api.v1.dto.DiscordGuild;
 
 import java.io.IOException;
 import java.net.http.HttpResponse;
@@ -9,13 +10,7 @@ public class Status {
     public static HttpResponse<String> addGuild(Guild guild) throws IOException, InterruptedException {
         String url = Http.baseUrl + "status/guild/add";
 
-        // TODO: Properly serialize json
-        String json = "{"
-                + "\"id\":\"" + guild.getId().asString() + "\","
-                + "\"name\":\"" + guild.getName() + "\","
-                + "\"memberCount\":" + guild.getMemberCount()
-                + "}";
-
+        String json = Http.MAPPER.writeValueAsString(DiscordGuild.fromNativeGuild(guild));
         HttpResponse<String> resp = Http.makePutRequest(url, json);
 
         if (resp.statusCode() >= 400) {
--- src/main/java/net/iotku/subdonic/api/v1/dto/DiscordGuild.java
+++ src/main/java/net/iotku/subdonic/api/v1/dto/DiscordGuild.java
@@ -1,3 +1,13 @@
 package net.iotku.subdonic.api.v1.dto;
 
-public record DiscordGuild(String id, String name, int memberCount) {}
+import discord4j.core.object.entity.Guild;
+
+public record DiscordGuild(Long id, String name, int memberCount) {
+    public static DiscordGuild fromNativeGuild(Guild guild) {
+        return new DiscordGuild(
+                guild.getId().asLong(),
+                guild.getName(),
+                guild.getMemberCount()
+        );
+    }
+}
