f3be0feb0d833b75e554c12fae1a0a7e06515bb8
Jonathan A. <me@iotku.pw>
2025-11-11 19:25:24 -0800

    feat: add DEFAULT Song record for a placeholder value

--- src/main/java/net/iotku/subdonic/api/v1/dto/Song.java
+++ src/main/java/net/iotku/subdonic/api/v1/dto/Song.java
@@ -4,4 +4,11 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record Song(String title, String artist, String album, String year, String id) {
+    public static final Song DEFAULT = new Song(
+            "No song playing",
+            "",
+            "",
+            "",
+            ""
+    );
 }
